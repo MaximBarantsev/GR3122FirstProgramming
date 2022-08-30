136901f6b60e690b41f06d146073dbde45437066
Defiler <m.barancev@gmail.com>
2022-08-30 22:46:05 +0300

    task 0 task 1 task 3 task 5 task 7

---
+++ Seminar1Task0/Program.cs
@@ -0,0 +1,13 @@
+﻿//============================================================================
+// # 0 Напишите программу, которая на входи принимает число и выдает его кавадрат
+//(число умноженное на само себя).
+//============================================================================
+
+string? inputLine = Console.ReadLine();
+if (inputLine != null)
+{
+    int inputNumber = int.Parse(inputLine);
+    int outNumber = inputNumber * inputNumber;
+    //int outNumber = (int)Math.Pow(inputNumber,2);
+    Console.WriteLine(outNumber);
+}
---
+++ Seminar1Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿//============================================================================
+// # 0 Напишите программу, которая на входи принимает число и 
+//проверяет, является ли первое число квадратом второго.
+//============================================================================
+
+string? inputLineA = Console.ReadLine();
+string? inputLineA = Console.ReadLine();
+if (inputLineA != null && inputLineB != null)
+{
+    int inputNumberA = int.Parse(inputLineA);
+    int inputNumberB = int.Parse(inputLineB);
+
+    //bool outResult = (inputLineA * inputLineA == inputNumberB);
+    //bool outResult = (inputNumberA * inputNumberA == inputNumberB);
+    bool outResult = (Math.Sqrt(inputNumberB) == inputNumberA);
+
+    Console.WriteLine(outResult);
+}
