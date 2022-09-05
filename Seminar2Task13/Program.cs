162acd4b216618b8c3c027aa48d1b4eb86b7a58e
Defiler <m.barancev@gmail.com>
2022-09-05 17:59:13 +0300

    fix

--- Seminar2Task13/Program.cs
+++ Seminar2Task13/Program.cs
@@ -4,12 +4,32 @@
 // числа или сообщает, что третьей цифры нет.
 //================================================================
 
-Console.Write("Введите число: ");                                                    //Комментарий ко вводу
-string number = Console.ReadLine()??"";                                              //Вводим число
+string number = string.Empty;
+char[] charArray = {};
 
-char[] charArray = number.ToString().ToCharArray();                                  //Преобразуем строку в массив символов
 
-Console.WriteLine(charArray.Length >= 3 ? (charArray[2]) : ("Третьей цифры нет."));  //Вывод результата в консоль через тернарный оператор
+ReadData();
+CalculateData();
+PrintData();
+
+void ReadData()
+{
+    Console.Write("Введите число: ");                                                    //Комментарий ко вводу
+    number = Console.ReadLine() ?? "";                                                   //Вводим число
+}
+
+void CalculateData()
+{
+    charArray = number.ToString().ToCharArray();                                         //Преобразуем строку в массив символов
+                                
+}
+
+void PrintData()
+{
+    Console.WriteLine(charArray.Length >= 3 ? (charArray[2]) : ("Третьей цифры нет."));  //Вывод результата в консоль через тернарный оператор
+}
+
+
 
 
 // if (charArray.Length == 3)                                                        //Проверка на размер массива
@@ -22,38 +42,38 @@ Console.WriteLine(charArray.Length >= 3 ? (charArray[2]) : ("Третьей ци
 // }
 
 
-//Вариант с Логарифмом
-//===================================================================================
-int inputNumber = 0;
-int result = 0;
-ReadData();
-Console.WriteLine("Вариант 1: Цикл");
-CalculateData1();
-PrintData();
-Console.WriteLine("Вариант 2: Логарифм");
-CalculateData2();
-PrintData();
-// Ввод: любое число
-void ReadData()
-{
-Console.Write("Введите число: ");
-string? inputLine = Console.ReadLine();
-if (inputLine != null) inputNumber = int.Parse(inputLine);
-}
-// Вычисление: третья цифра, если есть. Вариант 1: цикл.
-void CalculateData1()
-{
-int temp = inputNumber;
-while (temp >= 1000) temp = temp / 10;
-result = inputNumber < 100 ? -1 : temp % 10;
-}
-// Вычисление: третья цифра, если есть. Вариант 2: логарифм.
-void CalculateData2()
-{
-int powNum = (int) Math.Log10(inputNumber);
-result = powNum > 1? inputNumber / (int) Math.Pow(10, powNum - 2) % 10 : -1;
-}
-// Вывод: результат на консоль
-void PrintData() {
-Console.WriteLine(result != -1 ? result : "третьей цифры нет");
-}
+// //Вариант с Логарифмом (Чужой)
+// //===================================================================================
+// int inputNumber = 0;
+// int result = 0;
+// ReadData();
+// Console.WriteLine("Вариант 1: Цикл");
+// CalculateData1();
+// PrintData();
+// Console.WriteLine("Вариант 2: Логарифм");
+// CalculateData2();
+// PrintData();
+// // Ввод: любое число
+// void ReadData()
+// {
+// Console.Write("Введите число: ");
+// string? inputLine = Console.ReadLine();
+// if (inputLine != null) inputNumber = int.Parse(inputLine);
+// }
+// // Вычисление: третья цифра, если есть. Вариант 1: цикл.
+// void CalculateData1()
+// {
+// int temp = inputNumber;
+// while (temp >= 1000) temp = temp / 10;
+// result = inputNumber < 100 ? -1 : temp % 10;
+// }
+// // Вычисление: третья цифра, если есть. Вариант 2: логарифм.
+// void CalculateData2()
+// {
+// int powNum = (int) Math.Log10(inputNumber);
+// result = powNum > 1? inputNumber / (int) Math.Pow(10, powNum - 2) % 10 : -1;
+// }
+// // Вывод: результат на консоль
+// void PrintData() {
+// Console.WriteLine(result != -1 ? result : "третьей цифры нет");
+// }
